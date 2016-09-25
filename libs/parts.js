af0386002ca53a42834a7583dfbc553a7cf5e7d6
Henrik Raitasola <henrik.raitasola@gmail.com>
2016-09-25 20:00:30 +0300

    Fix lint errors

--- libs/parts.js
+++ libs/parts.js
@@ -1,34 +1,30 @@
 const webpack = require('webpack');
 
-exports.devServer = function() {
-  return {
-    devServer: {
-      historyApiFallback: true,
-      hot: true,
-      inline: true,
-      stats: 'errors-only',
-      host: '0.0.0.0',
-      watchOptions: {
-        aggregateTimeout: 300,
-        poll: 1000
-      }
+exports.devServer = () => ({
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    stats: 'errors-only',
+    host: '0.0.0.0',
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000,
     },
-    plugins: [
-      new webpack.HotModuleReplacementPlugin({multiStep: true})
-    ]
-  };
-}
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin({ multiStep: true }),
+  ],
+});
 
-exports.setupCSS = function(paths) {
-  return {
-    module: {
-      loaders: [
-        {
-          test: /\.css$/,
-          loaders: ['style-loader', 'css-loader?sourceMap'],
-          include: paths
-        }
-      ]
-    }
-  }
-}
+exports.setupCSS = paths => ({
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader?sourceMap'],
+        include: paths,
+      },
+    ],
+  },
+});
