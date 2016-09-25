c68b6ddb6c7456d3580deacfcaeaa753c5e364d0
Henrik Raitasola <henrik.raitasola@gmail.com>
2016-09-25 21:37:59 +0300

    Remove unneeded file name

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const repoName = require('git-repo-name');
 const parts = require('./libs/parts');
 
 const PATHS = {
-  app: path.join(__dirname, 'src', 'index.jsx'),
+  app: path.join(__dirname, 'src'),
   style: path.join(__dirname, 'style'),
   build: path.join(__dirname, 'build'),
 };
