c94eb45a86ded824889651a048795da3281ce4a5
Henrik Raitasola <henrik.raitasola@gmail.com>
2016-09-25 19:34:57 +0300

    Fix wrong import

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { reducers, default as nextReducer } from './reducers';
+import reducers, { default as nextReducer } from './reducers';
 
-import App from './components/App';
+import App from './components/App.jsx';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 const store = createStoreWithMiddleware(reducers);
